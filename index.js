555812004c6a96045f71588c28fb8cc693e4875c
victor@victor <victor.dmaina@gmail.com>
2024-04-03 17:20:59 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function  submitData(){
+    const formData = {
+        name: `{$name}`,
+        email: `{$email}`
+      };
+    const config = {
+        method: "POST",
+        headers:{
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+            },
+    
+             body: JSON.stringify
+             ({ name: "Steve", email: "steve@steve.com" })
+               };
+    
+    return  fetch('http://localhost:3000/users', config)
+              .then((res)=>  res.json())
+              .then(data => document.body.innerHTML = data.id)
+              .catch(error => document.body.innerHTML = error.message)
+          }
+        
